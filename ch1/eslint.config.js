134febaa9dae5b07f264efb561ba62a6d0bae252
ntmack1 <ntmack5@gmail.com>
2026-03-13 19:32:07 -0500

    setup ESlint, Prettier, Husky

--- ch1/eslint.config.js
+++ ch1/eslint.config.js
@@ -1,27 +1,48 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import prettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  // Equivalent of .eslintignore
+  globalIgnores(['dist', 'vite.config.js']),
+
   {
     files: ['**/*.{js,jsx}'],
+
+    // Equivalent of "extends": [...]
     extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+      js.configs.recommended, // eslint:recommended
+      react.configs.flat.recommended, // plugin:react/recommended
+      react.configs.flat['jsx-runtime'], // plugin:react/jsx-runtime
+      jsxA11y.flatConfigs.recommended, // plugin:jsx-a11y/recommended
+      prettier, // prettier
+      reactHooks.configs.flat.recommended, // plugin:react-hooks/recommended
+      reactRefresh.configs.vite, // plugin:react-refresh
     ],
+
+    // Equivalent of env + parserOptions
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
       },
     },
+
+    // Equivalent of settings: { react: { version: "detect" } }
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    // Your existing rules + anything the book adds
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
