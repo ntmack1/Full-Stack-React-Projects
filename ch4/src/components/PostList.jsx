aa987d8833a303281a9e9b613c80fa7cddcabe32
ntmack1 <ntmack5@gmail.com>
2026-04-11 21:56:29 -0500

    feat: add blog components

---
+++ ch4/src/components/PostList.jsx
@@ -0,0 +1,17 @@
+import { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { Post } from './Post.jsx'
+
+export function PostList({ posts = [] }) {
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post {...post} key={post._id} />
+      ))}
+    </div>
+  )
+}
+
+PostList.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.shape(Post.propTypes)).isRequired,
+}
---
+++ ch4/src/components/PostSorting.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+
+export function PostSorting({ fields = [] }) {
+  return (
+    <div>
+      <label htmlFor='sortBy'>Sort By: </label>
+      <select name='sortBy' id='sortBy'>
+        {fields.map((field) => (
+          <option key={field} value={field}>
+            {field}
+          </option>
+        ))}
+      </select>
+      {' / '}
+      <label htmlFor='sortOrder'>
+        Sort Order:
+        <select name='sortOrder' id='sortOrder'>
+          <option value={'ascending'}>ascending</option>
+          <option value={'descending'}>descending</option>
+        </select>
+      </label>
+    </div>
+  )
+}
+
+PostSorting.propTypes = {
+  fields: PropTypes.arrayOf(PropTypes.string).isRequired,
+}
